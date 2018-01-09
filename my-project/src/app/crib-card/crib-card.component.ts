f7be87790d9effc0eb1b830bd6e896764a9ead6c
Angjel Spasovski <angjel.spasovski@gmail.com>
2018-01-09 14:10:53 +0100

    "Property Binding and Displaing data in different component"

---
+++ my-project/src/app/crib-card/crib-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-crib-card',
+  templateUrl: './crib-card.component.html',
+  styleUrls: ['./crib-card.component.css']
+})
+export class CribCardComponent implements OnInit {
+
+	/* Input called crib asign it to property called crib assign to any type */
+	/* Accept binding called crib and assign it to a local property here called crib as well */
+	@Input('crib') crib: any;
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
